105654bba073827d36fcc59650a188a7818b199a
fadelrio <delriofrancisco03@gmail.com>
2023-06-07 21:14:38 -0300

    typos y nueva función en resorte.h

--- nodo.h
+++ nodo.h
@@ -4,31 +4,34 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#define RESORTES_MAXIMOS 5
+
 typedef struct nodo nodo_t;
 typedef struct resorte resorte_t;
 
 nodo_t *nodo_crear(float posicion[2], bool es_fijo);
 
+//no se ocupa de liberar los resortes asociados
 void nodo_destruir(nodo_t *nodo);
 
 //float obtener_masa(nodo_t nodo); Hace falta que los nodos conozcan su propia masa???
 
 //agrega un resorte a los asociados al nodo
-bool nodo_agregar_resorte(resorte_t *resorte, nodo_t nodo);
+bool nodo_agregar_resorte(resorte_t *resorte, nodo_t *nodo);
 
-//elimina un resorte de los asociados al nodo
-bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t nodo);
+//elimina un resorte de los asociados al nodo. devuelve false si el resorte no estaba vinculado al nodo
+bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo);
 	
 //devuelve un puntero a la lista de resortes de la estructura (en memoria nueva o en la misma?)
-resorte_t **nodo_obtener_resortes(nodo_t nodo);
+resorte_t **nodo_obtener_resortes(const nodo_t *nodo);
 
 //devuelve la cantidad de resortes unidos al nodo
-size_t nodo_obtener_cantidad_de_resortes(nodo_t nodo);
+size_t nodo_obtener_cantidad_de_resortes(const nodo_t *nodo);
 
 //devuelve por interfaz a posicion un vector de dos elementos donde el primero corresponde a las x y el segundo a las y.
-void nodo_obtener_posicion(nodo_t nodo, float posicion[2]);
+void nodo_obtener_posicion(const nodo_t *nodo, float posicion[2]);
 
 //devuelve si el nodo es fijo o no
-bool nodo_es_fijo(nodo_t nodo);
+bool nodo_es_fijo(const nodo_t *nodo);
 
 #endif
--- resorte.h
+++ resorte.h
@@ -8,15 +8,18 @@ typedef struct nodo nodo_t;
 //nodo1 y nodo2 no pueden ser NULL
 resorte_t *resorte_crear(nodo_t *nodo1, nodo_t *nodo2);
 
-void resorte_destruir(resorte_t resorte);
+void resorte_destruir(resorte_t *resorte);
 
 //devuelve la constante k actual del resorte
-float resorte_obtener_constante(resorte_t resorte);
+float resorte_obtener_constante(resorte_t *resorte);
 
 //devuelve la longitud actual del resorte
-float resorte_obtener_longitud(resorte_t resorte);
+float resorte_obtener_longitud(resorte_t *resorte);
 
 //devuelve un arreglo con los dos nodos por interfaz
-void resorte_obtener_nodos(resorte_t resorte, nodo_t **nodos);
+void resorte_obtener_nodos(resorte_t *resorte, nodo_t **nodos);
+
+//devuelve si resorte1 es igual a resorte2
+bool resorte_comparar(resorte_t *resorte1, resorte_t *resorte2);
 
 #endif
