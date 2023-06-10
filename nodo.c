d7703dde8d99340f44bc37d2cfcec8cd18d20682
fadelrio <delriofrancisco03@gmail.com>
2023-06-09 22:11:51 -0300

    Todas las funciones de nodo.h implementadas en nodo.c, compila sin warnings pero sigue sin estar testeado

--- nodo.c
+++ nodo.c
@@ -4,18 +4,20 @@
 #include <string.h>
 
 
-typedef struct nodo{
+struct nodo{
 	//float masa; depende de la respuesta a la pregunta del header
 	resorte_t **resortes;//es NULL si nres es 0
+	size_t resortes_memb;//cantidad de memoria almacenada para la lista de resortes
 	size_t nres;//cantidad de resortes
 	bool es_fijo;
 	float posicion[2];//primer valor corresponde a la coordenada x & el segundo valor corresponde a la cordenada y
-} nodo_t;
+};
 
 
 nodo_t *nodo_crear(float posicion[2], bool es_fijo){
 	nodo_t *nodo = malloc(sizeof(nodo_t));
 	nodo->resortes = NULL;
+	nodo->resortes_memb = 0;
 	nodo->nres = 0;	
 	nodo->es_fijo = es_fijo;
 	memcpy(nodo->posicion, posicion, 2 * sizeof(float));
@@ -27,20 +29,27 @@ void nodo_destruir(nodo_t *nodo){
 }
 
 bool nodo_agregar_resorte(resorte_t *resorte, nodo_t *nodo){
-	if (nodo->nres == 0){
-		nodo->resortes = malloc(RESORTES_MAXIMOS * sizeof(resorte_t*));
+	if (nodo->resortes_memb == 0){
+		nodo->resortes = malloc(10 * sizeof(resorte_t*));
 		if (nodo->resortes == NULL)
 			return false;
+		nodo->resortes_memb = 10;
 	}
-	if (nodo->nres == RESORTES_MAXIMOS)
-		return false;
-	nodo->resortes[nodo->nres++];
+	if (nodo->nres == nodo->resortes_memb){
+		resorte_t **aux = realloc(nodo->resortes,((nodo->resortes_memb * 1.5) * sizeof(resorte_t*)));
+		if (aux == NULL)
+			return false;
+		free(nodo->resortes);
+		nodo->resortes = aux;
+		nodo->resortes_memb *= 1.5;
+	}
+	nodo->resortes[nodo->nres++] = resorte;
 	return true;
 }
 
-bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo){ //Hay que buscar una forma de solucionarlo sin incluir resorte.h porque queda recursivo
-/*	size_t i = 0;	
-	for (;resorte_comparar(resorte, nodo->resortes[i]) && i < nodo->nres ; i++);
+bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo, bool (*comparador)(resorte_t*, resorte_t*)){ 
+	size_t i = 0;	
+	for (;comparador(resorte, nodo->resortes[i]) && i < nodo->nres ; i++);
 	if (i == nodo->nres)
 		return false;
 	for (;i<nodo->nres;i++){
@@ -48,11 +57,11 @@ bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo){ //Hay que buscar u
 	}
 	nodo->nres--;
 	return true;
-*/
+
 }
 
 resorte_t **nodo_obtener_resortes(const nodo_t *nodo){
-//depende de la pregunta del header
+	return nodo->resortes;
 }
 
 size_t nodo_obtener_cantidad_de_resortes(const nodo_t *nodo){
--- nodo.h
+++ nodo.h
@@ -14,15 +14,13 @@ nodo_t *nodo_crear(float posicion[2], bool es_fijo);
 //no se ocupa de liberar los resortes asociados
 void nodo_destruir(nodo_t *nodo);
 
-//float obtener_masa(nodo_t nodo); Hace falta que los nodos conozcan su propia masa???
-
 //agrega un resorte a los asociados al nodo
 bool nodo_agregar_resorte(resorte_t *resorte, nodo_t *nodo);
 
 //elimina un resorte de los asociados al nodo. devuelve false si el resorte no estaba vinculado al nodo
-bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo);
+bool nodo_eliminar_resorte(resorte_t *resorte, nodo_t *nodo, bool (*comparador)(resorte_t*, resorte_t*));
 	
-//devuelve un puntero a la lista de resortes de la estructura (en memoria nueva o en la misma?)
+//devuelve un puntero a la lista de resortes de la estructura
 resorte_t **nodo_obtener_resortes(const nodo_t *nodo);
 
 //devuelve la cantidad de resortes unidos al nodo
